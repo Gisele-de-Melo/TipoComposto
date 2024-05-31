faebc946207beaa9b368f59e362cfc921aee611c
Gisele de Melo <gisele_demelo@hotmail.com>
2024-05-31 10:15:46 -0300

    tipo composto, blog codedelphi

--- Unit1.pas
+++ Unit1.pas
@@ -1,3 +1,15 @@
+//------------------------------------------------------------------------------------------------------------
+//********* Sobre ************
+//Autor: Gisele de Melo
+//Esse código foi desenvolvido com o intuito de aprendizado para o blog codedelphi.com, portanto não me
+//responsabilizo pelo uso do mesmo.
+//
+//********* About ************
+//Author: Gisele de Melo
+//This code was developed for learning purposes for the codedelphi.com blog, therefore I am not responsible for
+//its use.
+//------------------------------------------------------------------------------------------------------------
+
 unit Unit1;
 
 interface
